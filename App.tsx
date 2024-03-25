f11b782a4fbee0d4379b77c6d94787eb37a33d49
gutodidonato <rm99433@fiap.com.br>
2024-03-25 17:25:23 -0300

    iniciei o app, não possuo vr aqui

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./src/crono.png")} />
+      <View>
+        <TouchableOpacity>
+          <Text></Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text></Text>
+        </TouchableOpacity>
+      </View>
+      <Text>Tempo atual: 00:00:03 </Text>
     </View>
   );
 }
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
